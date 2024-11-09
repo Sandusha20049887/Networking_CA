bd01a8359233d18df248bc24d5b89fe0f0a5ce64
Sandusha20049887 <sandusha1997@gmail.com>
2024-11-09 11:03:44 +0000

    docker install script

---
+++ install-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update the package index
+sudo apt-get update -y
+
+
+
+# Update the package database
+sudo apt-get update
+
+# Install prerequisites
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Install Docker
+sudo apt-get install -y docker.io
+
+# Start Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
