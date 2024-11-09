9fa81d6cded6f2fae7cb2dd8dfcab29b50270f60
Sandusha20049887 <sandusha1997@gmail.com>
2024-11-09 11:37:40 +0000

    change docker script

--- install-docker.sh
+++ install-docker.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
 # Update the package index
 sudo apt-get update -y
-
-# Update the package database
-sudo apt-get update
 
 # Install prerequisites
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Install Docker
-sudo apt-get install -y docker.io
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Start Docker service
-sudo systemctl start docker
+# Set up the Docker stable repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
+
+# Enable Docker to start on boot
 sudo systemctl enable docker
+sudo systemctl start docker
